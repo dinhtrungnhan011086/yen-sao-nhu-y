259bcb7f3d49b2ee9e1e5f148b1f91e894ff82e5
Đinh Trung Nhân <dinhtrungnhan@gmail.com>
2026-04-25 15:00:56 +0700

    Add GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App'
 import { AdminAuthProvider } from './context/AdminAuthContext'
 import { SiteContentProvider } from './context/SiteContentContext'
 import './styles.css'
 
+const Router = window.location.hostname.endsWith('github.io')
+  ? HashRouter
+  : BrowserRouter
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AdminAuthProvider>
       <SiteContentProvider>
-        <BrowserRouter
+        <Router
+          basename={import.meta.env.BASE_URL}
           future={{
             v7_relativeSplatPath: true,
             v7_startTransition: true,
           }}
         >
           <App />
-        </BrowserRouter>
+        </Router>
       </SiteContentProvider>
     </AdminAuthProvider>
   </React.StrictMode>,
